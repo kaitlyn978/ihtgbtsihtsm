a54f35666e37e18986fde64f5341c8f619eedebf
kaitlyn978 <76715226+kaitlyn978@users.noreply.github.com>
2022-05-10 19:56:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//https://teachablemachine.withgoogle.com/models/IHscyawsS/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+    });
+    camera=document.getElementById("camera");
+Webcam.attach('#camera');
